cb44c349bfb31ca6c2e3d14629e56e9415bb2060
Robert-Gabriel Trifan <trifangrobert@gmail.com>
2022-01-11 13:51:54 +0200

    7/7 tests passed

--- risc/main.py
+++ risc/main.py
@@ -1,9 +1,9 @@
 only_ones = (2 ** 32) - 1
 filename = "./tests/rv32um-v-rem.mc"
-
 def instructionFetch():
     global programCounter
     currentInstruction = instructions[getIndex(programCounter)][1]
+    # print(len(currentInstruction))
     programCounter += 4
     return currentInstruction
 
@@ -73,10 +73,25 @@ def I_format(currentInstruction):
     elif opcode == '1110011':
         if funct3 == 0: #ECALL
             running = False
+    elif opcode == '0000011':
+        if funct3 == 2:
+            if registers[rs1] + imm not in variables:
+                print('ce ai facut sefu')
+            else:
+                registers[rd] = variables[registers[rs1] + imm]
 
 
 def S_format(currentInstruction):
-    return True
+    opcode = currentInstruction[-7:]
+    funct3 = int(currentInstruction[-15:-12], 2)
+    rs1 = int(currentInstruction[-20:-15], 2)
+    rs2 = int(currentInstruction[-25:-20], 2)
+    imm = int(currentInstruction[-31:-25] + currentInstruction[-12:-7], 2)
+    if currentInstruction[0] == '1':
+        imm -= 2 ** 11
+    if opcode == '0100011': # SW
+        if funct3 == 2:
+            variables[registers[rs1] + imm] = registers[rs2]
 
 
 def B_format(currentInstruction):
@@ -111,6 +126,11 @@ def U_format(currentInstruction):
     if opcode == '0110111': # LUI
         registers[rd] = 0
         registers[rd] = ((imm * (2 ** 12)) & only_ones)
+    elif opcode == '0010111': #AUIPC
+        registers[rd] = 0
+        jump = (imm * (2 ** 12)) & only_ones
+        registers[rd] = programCounter + jump - 4
+        # print('AUIPC')
 
 
 def J_format(currentInstruction):
@@ -151,13 +171,27 @@ lines = input_file.readlines()
 lines = lines[1:]
 # print(lines)
 instructions = [x.split() for x in lines if x[9] != '<']
+readingData = False
+variables = {} # sunt un dictionar jmecher
 for x in instructions:
+    # print(x)
+    if '.data:' in x:
+        readingData = True
+        continue
     x[0] = x[0][:-1]
     x[0] = int(x[0], 16) - 8 * 16 ** 7
     x[1] = bin(int(x[1], 16))
     x[1] = x[1][2:]
     x[1] = ('0' * (32 - len(x[1]))) + x[1]
+    # print(x)
+    if readingData == True:
+        if x[0] % 4 == 0:
+            variables[int(x[0])] = int(x[1], 2)
+        else:
+            variables[int(x[0]) - 2] = variables[int(x[0]) - 2] * (2 ** 16) + int(x[1], 2)
     # print(x[1])
+
+
 
 
 # am sarit peste primele 31 de instructiuni pentru ca era initializarea registrilor cu 0
@@ -166,11 +200,17 @@ registers = [0] * 31
 instructions = instructions[32:]
 programCounter = 10720
 
+
 running = True
-while running == True:
-    print(programCounter)
+steps = 100000
+while running == True and steps > 0:
     # print(registers)
     currentInstruction = instructionFetch()
     instructionDecode(currentInstruction)
+    steps -= 1
 
-print(registers)
+if steps == 0: #infinite loop
+    print('failed')
+else:
+    print('passed')
+# print(registers)
